053fff6a3a49e5f3b3420375954ac314e0b3ab52
Nega García <nega_gl@outlook.com>
2023-08-16 17:38:01 -0500

    Finished project

--- main.py
+++ main.py
@@ -1,16 +1,51 @@
-# This is a sample Python script.
+# First we welcome the player
+print("Welcome to this mini-quiz about League of Legends!!!")
 
-# Press Mayús+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Wants to play?
+playing = input("Do you want to play? (y/n): ").lower()
+if playing != "y" and playing != "yes":
+    if playing == "n" or playing == "no":
+        print("You typed '" + playing + "'. Quitting...")
+        quit()
+    else:
+        print("You typed '" + playing + "'. That's not a valid choice. Quitting...")
+        quit()
 
+# Creating point system
+point = 0
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# If playing then we ask the questions
+answer = input("Which programming language is this quiz built on?: ").lower()
+if answer == 'python':
+    print("That's right!!! One point to griffyndor")
+    point += 1
+else:
+    print("Ugh, that's incorrect ):")
 
+# And more questions
+# Question 1
+answer = input("Last one was not a LOL question hehe. What is my favorite league champion?: ").lower()
+if answer == 'thresh':
+    print("That's right!!! Love that one")
+    point += 1
+else:
+    print("Ugh, that's incorrect ): answer is Thresh")
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# Question 2
+answer = input("What is Ruined King's actual name?: ").lower()
+if answer == 'viego':
+    print("Correct! You rock on this")
+    point += 1
+else:
+    print("Oops, nope ):")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+#  Question 3
+answer = input("What's that one game mode which is played in a single rail?: ").lower()
+if answer == 'aram':
+    print("Correct!")
+    point += 1
+else:
+    print("Meh, no ):")
+
+print("Total questions answered correctly: " + str(point) + "/4")
+print("you got " + str(int((point * 100)/4)) + "% correct.")
